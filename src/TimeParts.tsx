495c39f36300eae007f2b39b2a97946e978b52fa
M A Alim <ma.alim@curix.ai>
2023-06-25 17:43:04 +0600

    add time parts

---
+++ src/TimeParts.tsx
@@ -0,0 +1,38 @@
+import React, { FC, useEffect, useRef, useState } from 'react'
+import './timeparts.css'
+
+export interface TimePartsProps {
+	open: boolean
+	onItemSelect: (arg0: string) => void
+	start: number
+	end: number
+}
+
+const TimeParts: FC<TimePartsProps> = ({ open, onItemSelect, start, end }) => {
+	const [timeParts, setTimeParts] = useState<string[]>([])
+	const listRef = useRef<HTMLDivElement>(null)
+
+	useEffect(() => {
+		if (listRef.current) listRef.current.style.display = open ? 'block' : 'none'
+	}, [open])
+
+	useEffect(() => {
+		let i
+		const parts: string[] = []
+		for (i=start;i<=end;i++) {
+			const j = i < 10 ? `0${i}` : `${i}`
+			parts.push(`${j}:00`)
+		}
+		setTimeParts(parts)
+	}, [start, end])
+
+	return (
+		<div className="parts-container" ref={listRef}>
+			{timeParts && timeParts.map((part, index) => (
+				<div key={index} className="parts-item" onClick={() => onItemSelect(part)}>{part}</div>
+			))}
+		</div>
+	)
+}
+
+export default TimeParts
--- src/TimePicker.tsx
+++ src/TimePicker.tsx
@@ -1,21 +1,45 @@
-import React, { FC, useState } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import './timepicker.css'
 import InputMask from './InputMask'
+import TimeParts from './TimeParts'
 
 export interface InputProps {
-	hour: string
-	minute: string
+	time?: Date
+	showList?: boolean
+	minHour?: number
+	maxHour?: number
 }
 
-const TimePicker: FC<InputProps> = ({ hour, minute }) => {
-	const [hr, setHr] = useState(hour)
-	const [mn, setMn] = useState(minute)
+
+const TimePicker: FC<InputProps> = ({ time, showList = true, minHour = 0, maxHour = 23 }) => {
+	const [hr, setHr] = useState('')
+	const [mn, setMn] = useState('')
+	const [openList, setOpenList] = useState(false)
+
+	useEffect(() => {
+		if (!time) {
+			setHr((new Date().getHours()).toString())
+			setMn((new Date().getMinutes()).toString())
+		} else {
+			setHr((time.getHours()).toString())
+			setMn((time.getMinutes()).toString())
+		}
+	}, [time])
+
+	const setTime = (_time: string) => {
+		const parts = _time.split(':')
+		setHr(parts[0])
+		setMn(parts[1])
+		setOpenList(false)
+	}
 
 	return (
 		<div className="srt-timepicker">
-			<InputMask min={0} max={23} value={hr} onChangeCb={(v) => setHr(v)} />
+			<InputMask min={0 < minHour ? minHour : 0} max={23 > maxHour ? maxHour : 23} value={hr} onChangeCb={(v) => setHr(v)} />
 			<b>:</b>
 			<InputMask min={0} max={59} value={mn} onChangeCb={(v) => setMn(v)} />
+			{showList && <span className="chevron" onClick={() => setOpenList(!openList)}></span>}
+			<TimeParts open={openList} onItemSelect={setTime} start={minHour} end={maxHour} />
 		</div>
 	)
 }
